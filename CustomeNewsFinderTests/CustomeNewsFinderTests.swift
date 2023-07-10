418333e2660f03c0d6a0a12d5c89a843a9170586
alex <alexdok62@gmail.com>
2023-07-10 17:26:35 +0300

    +

--- CustomeNewsFinderTests/CustomeNewsFinderTests.swift
+++ CustomeNewsFinderTests/CustomeNewsFinderTests.swift
@@ -6,30 +6,31 @@
 //
 
 import XCTest
+@testable import CustomeNewsFinder
 
 final class CustomeNewsFinderTests: XCTestCase {
-
+    var sut: TableViewModel!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+       try super.setUpWithError()
+        sut = TableViewModel(networkManager: FakeNetworkManager())
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        try super.tearDownWithError()
+    }
+    
+    func testloadNewPageNews() {
+        // given
+        let page = sut.page
+
+        // when
+        sut.loadNewPageNews()
+
+        // then
+        XCTAssertEqual(sut.page, 2, "page is not next")
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
