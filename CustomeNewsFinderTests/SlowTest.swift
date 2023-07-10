98a4de8909c36d0097d4ad5b11c707c03a8eab11
alex <alexdok62@gmail.com>
2023-07-10 19:43:36 +0300

    add slow test

--- CustomeNewsFinderTests/SlowTest.swift
+++ CustomeNewsFinderTests/SlowTest.swift
@@ -1,35 +1,67 @@
-//
-//  SlowTest.swift
-//  CustomeNewsFinderTests
-//
-//  Created by алексей ганзицкий on 10.07.2023.
-//
-
 import XCTest
+@testable import CustomeNewsFinder
 
-final class SlowTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class FakesMapper: MapNewsToObject {
+    func map(_ news: News) -> [ObjectNewsData?] {
+        // Вернуть фейковые данные для тестирования
+        let objectNewsData = ObjectNewsData(author: "fake autor",title: "fake")
+        return [objectNewsData]
     }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+class FakeRequestBuilder: RequestBuilder {
+    func createRequestFrom(url: String, params: [String: String]) -> URLRequest? {
+        // Вернуть фейковый URLRequest для тестирования
+        let fakeURL = URL(string: "https://example.com/fake-request")!
+        let request = URLRequest(url: fakeURL)
+        return request
     }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+class NetworkManagerTests: XCTestCase {
+    
+    var networkManager: NetworkManager!
+    
+    override func setUp() {
+        super.setUp()
+        let mapper = FakesMapper()
+        let requestBuilder = FakeRequestBuilder()
+        networkManager = NetworkManagerImpl(mapper: mapper, requestBilder: requestBuilder)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    override func tearDown() {
+        networkManager = nil
+        super.tearDown()
+    }
+    
+    func testSendRequestForNews() {
+        // Given
+        let expectation = XCTestExpectation(description: "Retrieve news data")
+        let theme = "technology"
+        let page = 1
+        
+        // When
+        networkManager.sendRequestForNews(theme: theme, page: page) { newsData in
+            // Then
+            XCTAssertNotNil(newsData)
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 5.0)
     }
-
+    
+    func testLoadImage() {
+        // Given
+        let expectation = XCTestExpectation(description: "Load image")
+        let imageUrl = "https://picsum.photos/536/354"
+        
+        // When
+        networkManager.loadImage(urlForImage: imageUrl) { image in
+            // Then
+            XCTAssertNotNil(image)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
 }
